0abce911ef6109b3eb0f4d43128524bdd9acf5de
William Yeh <william.pjyeh@gmail.com>
2015-10-19 20:47:59 +0800

    Modify: remove the web8upd PPA.

---
+++ files/check-tarball-installed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# simple script to query the existance of specific Oracle jdk executable, if installed.
+#
+# @return: JSON string : { "found": true/false, "not_found": true/false }
+#
+
+PACKAGE="\"$1\""
+
+line=$(java -version 2>&1  | grep $PACKAGE | wc -l)
+#echo $line
+
+
+if [[ $line =~ 0 ]]; then
+  echo '{ "found": false , "not_found": true  }'
+
+else
+  echo '{ "found": true  , "not_found": false }'
+
+fi
